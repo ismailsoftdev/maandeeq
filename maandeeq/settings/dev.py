3ee291c24a88f750ac1294158ef6471750e5951b
ismailsoftdev <ismailsoftdev@gmail.com>
2024-07-29 12:32:41 +0300

    Fixed a product save error, also migrated using SQLite for the dev environment

--- maandeeq/settings/dev.py
+++ maandeeq/settings/dev.py
@@ -1,8 +1,6 @@
 from .base import *
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES['default'] = dj_database_url.config(
+    conn_max_age=600,
+    default=config('DATABASE_URL')
+)
